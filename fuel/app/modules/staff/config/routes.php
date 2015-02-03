fb20bb4b5544baf7b68701d0fec335f5b2e47607
Kaorun343 <kaoru.h65536@live.jp>
2015-02-04 07:27:10 +0900

    ルーティングを修正

--- fuel/app/modules/staff/config/routes.php
+++ fuel/app/modules/staff/config/routes.php
@@ -2,11 +2,14 @@
 return array(
 	'_root_'  => 'staff/index',
 	'staff/admin/lesson/(:num)' => 'staff/admin/lesson/index/$1',
-	'staff/admin/lesson/(:num)/question' => 'staff/admin/question/$1',
-	'staff/admin/lesson/(:num)/question/(:num)' => 'staff/admin/question/$1/$2',
 
-	'staff/admin/lesson/(:num)/file' => 'staff/admin/file/$1',
-	'staff/admin/lesson/(:num)/file/(:num)' => 'staff/admin/file/$1/$2',
+	'staff/admin/lesson/(:num)/question' => 'staff/admin/question/index/$1',
+	'staff/admin/lesson/(:num)/question/(:num)' => 'staff/admin/question/index/$1/$2',
+
+	'staff/admin/lesson/(:num)/file' => 'staff/admin/file/index/$1',
+	'staff/admin/lesson/(:num)/file/(:num)' => 'staff/admin/file/index/$1/$2',
+
+	'staff/admin/download/(:num)' => 'staff/admin/download/index/$1',
 
 	'staff/api/lesson/(:num)/questions' => 'staff/api/questions/$1',
 	'staff/api/lesson/(:num)/result' => 'staff/api/result/$1',
